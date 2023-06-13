698f294d8754cd8c29ad95f30fc8eed7434bb563
@syabdulr <abdulsyed1998@gmail.com>
2023-06-13 16:55:37 -0400

    show the new task layout by pressing a button to show it and hide it

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -4,7 +4,7 @@ import Tasks from './components/Tasks';
 import AddTask from './components/AddTask'
 
 function App() {
-
+  const [showAddTask, setShowAddTask] = useState(true)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -55,8 +55,11 @@ function App() {
 
   return (
     <div className='container'>
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask
+        (!showAddTask)} 
+        showAdd={showAddTask}
+        />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete=
         {deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
